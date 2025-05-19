c6d1c0f1c2f7ca39ff530ba37cfada7ef7b2240b
kkk <rlarhkdrnjs02@gmail.com>
2025-05-20 04:10:36 +0900

    채팅 입력시, 답변 오기까지 입력한 채팅을 볼 수 없는 문제 수정

--- src/containers/ChatContainer/ChatList.tsx
+++ src/containers/ChatContainer/ChatList.tsx
@@ -40,19 +40,29 @@ export default function ChatList({
 	};
 	return (
 		<div className={['flex flex-col flex-1 overflow-y-auto', 'p-4'].join(' ')}>
-			{status.status === 'idle' &&
-				(conversation && conversation.chats.length > 0 ? (
-					conversation.chats.map(chat => makeChat(chat))
-				) : (
-					<div className="flex flex-1 justify-center items-center">
-						<p className="text-[#ececf1] text-xl font-bold">
-							오늘은 어떤 것이 궁금한가요?
-						</p>
-					</div>
-				))}
+			{
+				conversation &&
+					conversation.chats.length > 0 ?
+					(
+						conversation.chats.map(chat => makeChat(chat))
+					) : status.status === 'idle' &&
+					(
+						<div className="flex flex-1 justify-center items-center">
+							<p className="text-[#ececf1] text-xl font-bold">
+								오늘은 어떤 것이 궁금한가요?
+							</p>
+						</div>
+					)
+			}
 			{status.status === 'loading' && (
 				<>
-					{status.chat && makeChat(status.chat)}
+					{
+						makeChat({
+							subject: 'user',
+							turn: -1,
+							content: status.chat?.content || '',
+						})
+					}
 					{makeChat({
 						subject: 'assistant',
 						turn: -1,
@@ -62,7 +72,13 @@ export default function ChatList({
 			)}
 			{status.status === 'error' && (
 				<>
-					{status.chat && makeChat(status.chat)}
+					{
+						makeChat({
+							subject: 'user',
+							turn: -1,
+							content: status.chat?.content || '',
+						})
+					}
 					<div className="flex w-full justify-start py-1">
 						<div className="inline-block max-w-[80%] px-4 py-2 text-base bg-red-100 text-red-700 rounded-2xl rounded-bl-md shadow">
 							<p className="whitespace-pre-wrap break-words">
